0698dee476c22d9e19ce4c6e5396b992a0a12655
Andrew Glotzbach <21aglotzbach@lmclakers.org>
2018-01-19 19:37:54 -0500

    Implemented the XDrive.

--- src/Subsystems/DriveTrain.cpp
+++ src/Subsystems/DriveTrain.cpp
@@ -9,9 +9,10 @@
 #include "RobotMap.h"
 #include <WPILib.h>
 #include "Commands/DriveWithJoystick.h"
-
+#include <math.h>
 const double Pi = 3.1415926535;
 const double QuarterPi = Pi / 4.0;
+const double MaxRPM = 4000.0;
 
 DriveTrain::DriveTrain() : Subsystem("DriveTrain")
 {
@@ -33,4 +34,39 @@ void DriveTrain::InitDefaultCommand()
 void DriveTrain::XDrive(double magnitude, double totalAngle, double rotation)
 {
 	double theta = totalAngle % QuarterPi;
+
+	double mFinal = 1/cos(theta);
+
+	double motorA = -1*mFinal*magnitude*sin(totalAngle-QuarterPi);
+	double motorB = mFinal*magnitude*cos(totalAngle-QuarterPi);
+	double motorC = -motorA;
+	double motorD = -motorB;
+
+	motorA = motorA + rotation;
+	motorB = motorB + rotation;
+	motorC = motorC + rotation;
+	motorD = motorD + rotation;
+
+	//finds the maximum of 1 or motorA, motorB, motorC, and motorD
+	double maxCalcMagnitude = 1;
+	if (abs(motorA) > maxCalcMagnitude)
+		maxCalcMagnitude = abs(motorA);
+	if (abs(motorB) > maxCalcMagnitude)
+		maxCalcMagnitude = abs(motorB);
+	if (abs(motorC) > maxCalcMagnitude)
+		maxCalcMagnitude = abs(motorC);
+	if (abs(motorD) > maxCalcMagnitude)
+		maxCalcMagnitude = abs(motorD);
+
+	//scaling motor outputs by the maxCalcMagnitude
+	motorA = motorA / maxCalcMagnitude;
+	motorB = motorB / maxCalcMagnitude;
+	motorC = motorC / maxCalcMagnitude;
+	motorD = motorD / maxCalcMagnitude;
+
+	// scaling to RPMs and sending to the motors
+	m_FrontLeftMotor->Set(motorA * MaxRPM);
+	m_FrontRightMotor->Set(motorB * MaxRPM);
+	m_BackRightMotor->Set(motorC * MaxRPM);
+	m_BackLeftMotor->Set(motorD * MaxRPM);
 }
