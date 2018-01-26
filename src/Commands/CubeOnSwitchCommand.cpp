f40799402bfc232e2b6a91bb944749479e4f0aaf
Andrew Glotzbach <21aglotzbach@lmclakers.org>
2018-01-25 19:48:52 -0500

    Created the command "CubeOnSwitchCommand". Waiting for the program for the cube lift before writing the command.

---
+++ src/Commands/CubeOnSwitchCommand.cpp
@@ -0,0 +1,18 @@
+/*
+ * CubeOnSwitchCommand.cpp
+ *
+ *  Created on: Jan 25, 2018
+ *      Author: Tracy
+ */
+
+#include <Commands/CubeOnSwitchCommand.h>
+
+CubeOnSwitchCommand::~CubeOnSwitchCommand() {
+	// TODO Auto-generated destructor stub
+}
+
+CubeOnSwitchCommand::CubeOnSwitchCommand() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ src/Commands/CubeOnSwitchCommand.h
@@ -0,0 +1,17 @@
+/*
+ * CubeOnSwitchCommand.h
+ *
+ *  Created on: Jan 25, 2018
+ *      Author: Tracy
+ */
+
+#ifndef SRC_COMMANDS_CUBEONSWITCHCOMMAND_H_
+#define SRC_COMMANDS_CUBEONSWITCHCOMMAND_H_
+
+class CubeOnSwitchCommand {
+public:
+	virtual ~CubeOnSwitchCommand();
+	CubeOnSwitchCommand();
+};
+
+#endif /* SRC_COMMANDS_CUBEONSWITCHCOMMAND_H_ */
