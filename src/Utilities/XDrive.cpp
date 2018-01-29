a2a922b0ff4d8d16c2a0ec9d0f208e6a3896d4eb
cwtech2959 <cwtech2959@gamil.com>
2018-01-29 18:45:43 -0500

    changed KP added KI

--- src/Utilities/XDrive.cpp
+++ src/Utilities/XDrive.cpp
@@ -33,28 +33,32 @@ XDrive::XDrive()
 	m_frontLeftMotor->ConfigVelocityMeasurementWindow(1,0);
 	m_frontLeftMotor->SetSensorPhase(true);
 	m_frontLeftMotor->Config_kF(0, 0.275, 0);
-	m_frontLeftMotor->Config_kP(0, 0.3, 0);
+	m_frontLeftMotor->Config_kP(0, 0.45, 0);
+	m_frontLeftMotor->Config_kI(0, 0.0045, 0);
 
 	m_backLeftMotor->ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative, 0, 0);
 	m_backLeftMotor->ConfigVelocityMeasurementPeriod(VelocityMeasPeriod::Period_5Ms, 0);
 	m_backLeftMotor->ConfigVelocityMeasurementWindow(1,0);
 	m_backLeftMotor->SetSensorPhase(true);
 	m_backLeftMotor->Config_kF(0, 0.275, 0);
-	m_backLeftMotor->Config_kP(0, 0.3, 0);
+	m_backLeftMotor->Config_kP(0, 0.45, 0);
+	m_backLeftMotor->Config_kI(0, 0.0045, 0);
 
 	m_frontRightMotor->ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative, 0, 0);
 	m_frontRightMotor->ConfigVelocityMeasurementPeriod(VelocityMeasPeriod::Period_5Ms, 0);
 	m_frontRightMotor->ConfigVelocityMeasurementWindow(1,0);
 	m_frontRightMotor->SetSensorPhase(true);
 	m_frontRightMotor->Config_kF(0, 0.275, 0);
-	m_frontRightMotor->Config_kP(0, 0.3, 0);
+	m_frontRightMotor->Config_kP(0, 0.45, 0);
+	m_frontRightMotor->Config_kI(0, 0.0045, 0);
 
 	m_backRightMotor->ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative, 0, 0);
 	m_backRightMotor->ConfigVelocityMeasurementPeriod(VelocityMeasPeriod::Period_5Ms, 0);
 	m_backRightMotor->ConfigVelocityMeasurementWindow(1,0);
 	m_backRightMotor->SetSensorPhase(true);
 	m_backRightMotor->Config_kF(0, 0.275, 0);
-	m_backRightMotor->Config_kP(0, 0.3, 0);
+	m_backRightMotor->Config_kP(0, 0.45, 0);
+	m_backRightMotor->Config_kI(0, 0.0045, 0);
 
 	m_frontLeftMotor.get()->SetName("DriveTrain", "Front Left");
 	m_frontRightMotor.get()->SetName("DriveTrain", "Front Right");
