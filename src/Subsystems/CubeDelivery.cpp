f465219dc8d2761e6ce7309f9e0dcdc4d22a3b37
Andrew Glotzbach <21aglotzbach@lmclakers.org>
2018-02-03 13:12:37 -0500

    Rename

---
+++ src/Subsystems/CubeDelivery.cpp
@@ -0,0 +1,16 @@
+/*
+ * CubeDeliverySubsystem.cpp
+ *
+ *  Created on: Feb 3, 2018
+ *      Author: Andrew
+ */
+
+#include <Subsystems/CubeDelivery.h>
+
+CubeDelivery::CubeDelivery() : Subsystem("CubeDeliverySubsystem")
+{
+}
+
+void CubeDelivery::OpenArms()
+{
+}
