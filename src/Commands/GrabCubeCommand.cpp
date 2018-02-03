cf0aa1fc08abddc2d40f048b9beb4a78323a547f
Kevin Russell <kevinfrussell@gmail.com>
2018-02-03 12:55:05 -0500

    Started on the grab cube command

---
+++ src/Commands/GrabCubeCommand.cpp
@@ -0,0 +1,18 @@
+/*
+ * GrabCubeCommand.cpp
+ *
+ *  Created on: Feb 3, 2018
+ *      Author: Kevin
+ */
+
+#include <Commands/GrabCubeCommand.h>
+
+GrabCubeCommand::GrabCubeCommand() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GrabCubeCommand::~GrabCubeCommand() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Commands/GrabCubeCommand.h
@@ -0,0 +1,19 @@
+/*
+ * GrabCubeCommand.h
+ *
+ *  Created on: Feb 3, 2018
+ *      Author: Kevin
+ */
+
+#ifndef SRC_COMMANDS_GRABCUBECOMMAND_H_
+#define SRC_COMMANDS_GRABCUBECOMMAND_H_
+
+#include <Commands/Command.h>
+
+class GrabCubeCommand: public frc::Command {
+public:
+	GrabCubeCommand();
+	virtual ~GrabCubeCommand();
+};
+
+#endif /* SRC_COMMANDS_GRABCUBECOMMAND_H_ */
