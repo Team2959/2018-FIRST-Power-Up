e175c537d7703eba7dba76be77182f02a494f37e
Andrew Glotzbach <21aglotzbach@lmclakers.org>
2018-02-03 14:51:40 -0500

    More cube delivery

--- src/Subsystems/CubeDelivery.cpp
+++ src/Subsystems/CubeDelivery.cpp
@@ -6,11 +6,40 @@
  */
 
 #include <Subsystems/CubeDelivery.h>
+#include "RobotMap.h"
 
 CubeDelivery::CubeDelivery() : Subsystem("CubeDeliverySubsystem")
 {
+	m_cubeManipulationMotor.reset(new frc::Spark(CUBE_ARMS_OPEN_CLOSE_MOTOR));
 }
 
 void CubeDelivery::OpenArms()
 {
+	m_openCloseArms(frc::DoubleSolenoid::Value::kForward);
 }
+
+void CubeDelivery::CloseArms()
+{
+	m_openCloseArms(frc::DoubleSolenoid::Value::kReverse);
+}
+
+void CubeDelivery::StopWheels()
+{
+	m_cubeManipulationMotor->Set(0);
+}
+
+void CubeDelivery::SpinWheelsIn()
+{
+	m_cubeManipulationMotor->Set(1);
+}
+
+void CubeDelivery::SpinWheelsOutFast()
+{
+	m_cubeManipulationMotor->Set(-1);
+}
+
+void CubeDelivery::SpinWheelsOutSlow()
+{
+	m_cubeManipulationMotor->Set(-0.25);
+}
+
